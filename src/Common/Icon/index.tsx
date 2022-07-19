59d2d85d78261eb412b9858d6d1f2b9bdfead39f
YosKa <yoskadtl@gmail.com>
2022-07-19 11:43:31 +0300

    update all icon

---
+++ src/Common/Icon/index.tsx
@@ -0,0 +1,55 @@
+import { ReactComponent as IconFacebookLogo } from './Icons/logo-facebook.svg';
+import { ReactComponent as IconGoogleLogo } from './Icons/logo-google.svg';
+import { ReactComponent as IconYellowMoon } from './Icons/moon-flat.svg';
+import { ReactComponent as IconYellowSun } from './Icons/sun-flat.svg';
+import { ReactComponent as IconWhiteSun } from './Icons/sun-white.svg';
+import { ReactComponent as IconArrow } from './Icons/arrow.svg';
+import { ReactComponent as IconCheckV } from './Icons/check-v.svg';
+import { ReactComponent as IconFavFull } from './Icons/fav-full.svg';
+import { ReactComponent as IconFavOutLine } from './Icons/fav-outline.svg';
+import { ReactComponent as IconHomeFull } from './Icons/home-full.svg';
+import { ReactComponent as IconHomeOutLine } from './Icons/home-outline.svg';
+import { ReactComponent as IconInfoCircle } from './Icons/info-circle.svg';
+import { ReactComponent as IconLayot } from './Icons/layout.svg';
+import { ReactComponent as IconLogOUt } from './Icons/log-out.svg';
+import { ReactComponent as IconMapWeater } from './Icons/map-weather.svg';
+import { ReactComponent as IconMap } from './Icons/map.svg';
+import { ReactComponent as IconMenu } from './Icons/menu.svg';
+import { ReactComponent as IconDarkMOon } from './Icons/moon-dark.svg';
+import { ReactComponent as IconFlatMoon } from './Icons/moon-flat.svg';
+import { ReactComponent as IconWhiteMoon } from './Icons/moon-white.svg';
+import { ReactComponent as IconDarkSearch } from './Icons/search-dark.svg';
+import { ReactComponent as IconWhiteSearch } from './Icons/search-white.svg';
+import { ReactComponent as IconPlusSearch } from './Icons/search+.svg';
+import { ReactComponent as IconDarkSun } from './Icons/sun-dark.svg';
+import { ReactComponent as IconFlatSun } from './Icons/sun-flat.svg';
+
+
+
+export {
+    IconFlatSun,
+    IconDarkSun,
+    IconPlusSearch,
+    IconWhiteSearch,
+    IconDarkSearch,
+    IconWhiteMoon,
+    IconFlatMoon,
+    IconDarkMOon,
+    IconMenu,
+    IconMap,
+    IconMapWeater,
+    IconLogOUt,
+    IconLayot,
+    IconInfoCircle,
+    IconHomeOutLine,
+    IconHomeFull,
+    IconYellowMoon,
+    IconGoogleLogo,
+    IconFacebookLogo,
+    IconYellowSun,
+    IconWhiteSun,
+    IconArrow,
+    IconCheckV,
+    IconFavFull,
+    IconFavOutLine
+};
